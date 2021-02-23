488d387850fac5fc57ac0a146ab1606fc8e948d9
khemchandkolhe <khemchandkolhe3@gmail.com>
2021-02-22 18:21:41 -0800

    gggg

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+wdwddded
 
+rer
+e
+r
+er
+
+r
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
